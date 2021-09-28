4de709cf56e4905e4d67cbaf1fca0ebc85a5506d
J-MARR <72269208+J-MARR@users.noreply.github.com>
2021-09-27 20:57:27 -0400

    Add files via upload

---
+++ Task 2/t2.py
@@ -0,0 +1,17 @@
+""" Task 2: Drug Classification"""
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+
+"""Generating distribution graph of instances"""
+df = pd.read_csv(r"data\drug200.csv")
+target_counts = df["Drug"].value_counts()
+target_counts.plot.bar()
+plt.savefig("drug-distribution.pdf")
+
+"""Converting ordinal and nominal values"""
+"""...to be continued"""
+
+train, test = train_test_split(df)
+print(train)
