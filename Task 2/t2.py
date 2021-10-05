2c5a257ae1da59a1af1e33b1dadbfd5dfed2bb1d
J-MARR <72269208+J-MARR@users.noreply.github.com>
2021-10-05 12:55:09 -0400

    Create t2.py

--- Task 2/t2.py
+++ Task 2/t2.py
@@ -1,6 +1,7 @@
 """ Task 2: Drug Classification"""
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 import numpy as np
 import pandas as pd
 
@@ -11,7 +12,41 @@ target_counts.plot.bar()
 plt.savefig("drug-distribution.pdf")
 
 """Converting ordinal and nominal values"""
-"""...to be continued"""
+"""Nominal-to-numerical"""
+num_convert_partial = pd.get_dummies(df, columns=["Sex"])
 
-train, test = train_test_split(df)
-print(train)
+"""Ordinal-to-numerical"""
+replace_map = {"BP": {"HIGH":3, "NORMAL":2, "LOW":1}, "Cholesterol": {"HIGH":3, "NORMAL":2, "LOW":1}, "Drug": {"drugA":1, "drugB":2, "drugC":3, "drugX":4, "drugY":5}}
+num_convert = num_convert_partial.replace(replace_map)
+
+
+"""Split the data into sets"""
+train, test = train_test_split(num_convert)
+
+train_x = train[["Age", "BP", "Cholesterol", "Na_to_K", "Sex_F", "Sex_M"]]
+train_y = train[["Drug"]].values.ravel()
+test_x = test[["Age", "BP", "Cholesterol", "Na_to_K", "Sex_F", "Sex_M"]]
+test_y = test[["Drug"]].values.ravel()
+
+
+"""NB classifier"""
+gnb = GaussianNB()
+NB_y = gnb.fit(train_x,train_y).predict(test_x)
+
+print("GB mislabeled points: %d" % ((test_y != NB_y).sum()))
+
+"""Base-DT classifier"""
+
+
+"""Top-DT classifier"""
+
+
+"""PER classifier"""
+
+
+"""Base-MLP classifier"""
+
+
+"""Top-MLP classifier"""
+
+
